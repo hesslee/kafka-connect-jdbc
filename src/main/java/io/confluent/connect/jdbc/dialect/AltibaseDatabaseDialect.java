65b6808c39c3df3918307071fa4646b638852bdd
hesslee <hess.lee70@gmail.com>
2023-07-07 17:18:58 +0900

    Update AltibaseDatabaseDialect.java

--- src/main/java/io/confluent/connect/jdbc/dialect/AltibaseDatabaseDialect.java
+++ src/main/java/io/confluent/connect/jdbc/dialect/AltibaseDatabaseDialect.java
@@ -194,11 +194,4 @@ public class AltibaseDatabaseDialect extends GenericDatabaseDialect {
     builder.append(")");
     return builder.toString();
   }
-
-  @Override
-  protected String sanitizedUrl(String url) {
-    // DB2 has semicolon delimited property name-value pairs
-    return super.sanitizedUrl(url)
-                .replaceAll("(?i)([:;]password=)[^;]*", "$1****");
-  }
 }
